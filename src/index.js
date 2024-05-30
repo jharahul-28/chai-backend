d27851f9fd97641c518753430a1deadb67232766
jharahul-28 <rahuljha84211@gmail.com>
2024-05-30 14:15:56 +0530

    Connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import dotenv from "dotenv";
 // import { DB_NAME } from "./constants";
 // import express from "express";
 import ConnectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({path: './env'});
 
-ConnectDB();
+ConnectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running at PORT: ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("Mongo DB error failed!!", err);
+})
 
 
 
